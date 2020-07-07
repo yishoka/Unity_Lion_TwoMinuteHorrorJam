30db53f9b0ec6e3f2e48a575bbf66aa6093e2518
yishoka <63327438+yishoka@users.noreply.github.com>
2020-07-07 16:24:20 +0800

    HDRP

---
+++ TwoMinuteHorrorJam/Assets/Scripts/HorrorObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class HorrorObject : MonoBehaviour, ITriggerEvent
+{
+    /// <summary>
+    /// 允許子類別覆寫觸發事件
+    /// </summary>
+    public virtual void TriggerEvent()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "玩家") TriggerEvent();
+    }
+}
---
+++ TwoMinuteHorrorJam/Assets/Scripts/ITriggerEvent.cs
@@ -0,0 +1,11 @@
+﻿
+/// <summary>
+/// 介面：觸事件
+/// </summary>
+public interface ITriggerEvent
+{
+    /// <summary>
+    /// 觸發後要執行的事件
+    /// </summary>
+    void TriggerEvent();
+}
